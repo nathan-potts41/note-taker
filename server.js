5450c22b10b75321c4f19afa1664d327d6c1fad8
nathan-potts41 <pottsandpans41@hotmail.com>
2020-12-22 10:14:28 -0600

    Add heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const { notes } = require('./data/notes');
+
+// app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+});
+
+app.listen(port, () => {
+    console.log(`Now listening at http://localhost:${port}`)
+});
+
