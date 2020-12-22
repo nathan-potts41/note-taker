f1b6031b7754b1407331fa091e66f09981c788e1
nathan-potts41 <pottsandpans41@hotmail.com>
2020-12-22 10:22:05 -0600

    port changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const { notes } = require('./data/notes');
 
 // app.use(express.static('public'));
@@ -9,7 +9,7 @@ app.get('/api/notes', (req, res) => {
     res.json(notes)
 });
 
-app.listen(port, () => {
-    console.log(`Now listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Now listening at http://localhost:${PORT}`)
 });
 
